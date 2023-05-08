f3b963ef8bbdfdf9e4fde0bb64e2af50cfa47689
Casey Smith <csmith@morningstarcorp.com>
2023-05-08 14:17:46 -0400

    Update examples to match new methods of PassKey and Numeric Comparison

--- examples/basic/BLE_uart/main/main.cpp
+++ examples/basic/BLE_uart/main/main.cpp
@@ -59,6 +59,7 @@ class MyServerCallbacks: public BLEServerCallbacks {
     }
   /***************** New - Security handled here ********************
   ****** Note: these are the same return values as defaults ********/
+<<<<<<< HEAD
     uint32_t onPassKeyRequest(){
       printf("Server PassKeyRequest\n");
       return 123456;
@@ -67,6 +68,21 @@ class MyServerCallbacks: public BLEServerCallbacks {
     bool onConfirmPIN(uint32_t pass_key){
       printf("The passkey YES/NO number: %" PRIu32"\n", pass_key);
       return true;
+=======
+    uint32_t onPassKeyDisplay(){
+      printf("Server PassKeyDisplay\n");
+      return 123456; 
+    }
+
+    void onPassKeyEntry(const BLEAddress& address){
+      printf("Server PassKeyEntry\n");
+      BLEDevice::getServer()->injectPassKey(address, 123456);
+    }
+
+    void onConfirmPIN(const BLEAddress& address, uint32_t pass_key){
+      printf("The passkey YES/NO number: %d\n", pass_key);
+      BLEDevice::getServer()->injectConfirmPIN(address, true);
+>>>>>>> c721ae0 (Update examples to match new methods of PassKey and Numeric Comparison)
     }
 
     void onAuthenticationComplete(BLEConnInfo& connInfo){
