ac55482b187cc3d08617515a0df3a7b49ecaba5e
h2zero <ryan@nable-embedded.io>
2025-06-21 17:08:59 -0600

    Change default security settings to BLE secure connections off.
    Fixing some connection issues when enabled, users should enable if desired.

--- examples/NimBLE_Client/main/main.cpp
+++ examples/NimBLE_Client/main/main.cpp
@@ -261,8 +261,7 @@ extern "C" void app_main(void) {
      *  These are the default values, only shown here for demonstration.
      */
     // NimBLEDevice::setSecurityAuth(false, false, true);
-
-    NimBLEDevice::setSecurityAuth(/*BLE_SM_PAIR_AUTHREQ_BOND | BLE_SM_PAIR_AUTHREQ_MITM |*/ BLE_SM_PAIR_AUTHREQ_SC);
+    // NimBLEDevice::setSecurityAuth(BLE_SM_PAIR_AUTHREQ_BOND | BLE_SM_PAIR_AUTHREQ_MITM | BLE_SM_PAIR_AUTHREQ_SC);
 
     /** Optional: set the transmit power */
     NimBLEDevice::setPower(3); /** 3dbm */
