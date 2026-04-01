990c7c7f05425dd8dd3f3b88e971ecf38b793688
h2zero <h2zero@users.noreply.github.com>
2026-04-01 20:22:14 +0000

    deploy: e26b502297396401c166083ed66ebf5498c805a8

--- search/all_5.js
+++ search/all_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['5_20x_20features_0',['Bluetooth 5.x features',['../md__bluetooth_015_01features.html',1,'']]]
+  ['5_200_202026_2004_2001_0',['[2.5.0] 2026-04-01',['../md__2github_2workspace_2_c_h_a_n_g_e_l_o_g.html#autotoc_md250-2026-04-01',1,'']]],
+  ['5_20x_20features_1',['Bluetooth 5.x features',['../md__bluetooth_015_01features.html',1,'']]]
 ];
